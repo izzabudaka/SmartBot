92097fcd2e96e39c914fd6bec06550f9e75e4164
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2016-02-27 14:42:56 +0000

    Added initial Xcode project

---
+++ mobile-client/SmartBotApp/SmartBotApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartBotApp
+//
+//  Created by Leonardo Ciocan on 27/02/2016.
+//  Copyright © 2016 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
